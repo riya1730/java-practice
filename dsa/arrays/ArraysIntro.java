af96d22e1294f14a58cabff005edcaba2d5882ef
Riya Sewatkar <riyasewatkar06@gmail.com>
2025-12-31 03:01:22 +0530

    Day 2: Array basics and traversal

---
+++ dsa/arrays/ArraysIntro.java
@@ -0,0 +1,13 @@
+package dsa.arrays;
+
+public class ArraysIntro {
+    public static void main(String[] args) {
+
+        int[] toys = {10, 20, 30, 40};
+
+        System.out.println(toys[0]); // first toy
+        System.out.println(toys[1]); // second toy
+        System.out.println(toys.length); // how many toys
+    }
+}
+
